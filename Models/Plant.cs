ca61e3a132b5d1e80ae1342ab3fae98c12466667
Adela Darmansyah and Tiffany Siu <tiffany@>
2020-02-27 11:06:47 -0800

    Add game mechanics and Console UI

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -5,13 +5,75 @@ namespace Grow.Models
 {
   public class Plant
   {
-    private int _plantHealth;
-    private int _plantLevel;
+    public int FeedTurnNumber {get; set;}
+    public int WaterLevel {get; set;}
+    public int SunLevel {get; set;}
+    public int PlantHealth {get; set;}
+    public bool FeedAvailable {get; set;}
 
     public Plant()
     {
-      _plantHealth = 10;
-      _plantLevel = 1;
+      FeedTurnNumber = 0;
+      WaterLevel = 3;
+      SunLevel = 3;
+      PlantHealth = 2;
+      FeedAvailable = false;
+    }
+
+    public void Water()
+    {
+      WaterLevel += 2;
+    }
+
+    public void Feed()
+    {
+      Random random = new Random();
+      int element = random.Next(2);
+      int increaseLevel = random.Next(1, 4);
+      if (element == 0)
+      {
+        WaterLevel += increaseLevel;
+        SunLevel++;
+      } 
+      else 
+      {
+        WaterLevel++;
+        SunLevel += increaseLevel;
+      }
+      FeedAvailable = false;
+      FeedTurnNumber = 0;
+    }
+
+    public void GiveSunshine()
+    {
+      SunLevel += 2;
+    }
+
+    public void CheckFeed()
+    {
+      if (FeedTurnNumber >= 3) 
+      {
+        FeedAvailable = true;
+      } 
+      else 
+      {
+        FeedAvailable = false;
+      }
+    }
+
+    public void EndTurn()
+    {
+      WaterLevel--;
+      SunLevel--;
+      if (WaterLevel <= 0 || WaterLevel >= 5 || SunLevel <= 0 || SunLevel >= 5)
+      {
+        PlantHealth--;
+      }
+      else
+      {
+        PlantHealth = 2;
+      }
+      FeedTurnNumber++;
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,90 @@ namespace Grow
 {
   class Program
   {
-    static void Main()
-    {
+    public static Plant plant = new Plant();
+    public static bool gameOver = false;
 
+    public static void Main()
+    {
+      Console.WriteLine("===== Grow Your Plant =====");
+      Console.WriteLine("Keep your plant alive by");
+      Console.WriteLine("not letting the levels go");
+      Console.WriteLine("too high or low.");
+      while (!gameOver)
+      {
+        plant.CheckFeed();
+        DisplayStats();
+        MainMenu();
+        plant.EndTurn();
+        CheckGameOver();
+      }
+    }
+
+    public static void DisplayStats()
+    {
+      Console.WriteLine("===========================");
+      Console.WriteLine("==== Your Plant Stats =====");
+      Console.WriteLine($"Water Level: {plant.WaterLevel}/5");
+      Console.WriteLine($"Sun Level: {plant.SunLevel}/5");
+      Console.WriteLine($"Feed Available: {plant.FeedAvailable}");
+      Console.WriteLine("===========================");
+    }
+
+    public static void MainMenu()
+    {
+      Console.WriteLine("Choose an action: (Water/Sun/Feed/Nothing/Quit)");
+      string input = Console.ReadLine().ToLower();
+      if (input == "water")
+      {
+        plant.Water();
+      } 
+      else if (input == "sun")
+      {
+        plant.GiveSunshine();
+      } 
+      else if (input == "feed")
+      {
+        if (plant.FeedAvailable)
+        {
+          plant.Feed();
+        }
+        else
+        {
+          Console.WriteLine("Feed not available yet! Try again later.");
+          MainMenu();
+        }
+      } 
+      else if (input == "nothing")
+      {
+        Console.WriteLine("Nothing done this turn.");
+      }
+      else if (input == "quit")
+      {
+        Environment.Exit(0);
+      } 
+      else 
+      {
+        Console.WriteLine("Command not found. Please Try Again.");
+        MainMenu();
+      }
+    }
+
+    public static void CheckGameOver()
+    {
+      if (plant.PlantHealth == 1)
+      {
+        Console.WriteLine("Plant is dying! Watch your stats!");
+        gameOver = false;
+      } 
+      else if (plant.PlantHealth == 0)
+      {
+        Console.WriteLine("Plant has died! Game Over!");
+        gameOver = true;
+      }
+      else
+      {
+        gameOver = false;
+      }
     }
   }
 }
