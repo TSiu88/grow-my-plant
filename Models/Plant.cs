9cabe0686b81bc9c5e1f5c8802b8f530b335246f
Adela Darmansyah and Tiffany Siu <adela@>
2020-02-27 09:23:12 -0800

    Fix initial boilerplate code by renaming folders and files

---
+++ Models/Plant.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Grow.Models
+{
+  public class Plant
+  {
+    private int _plantHealth;
+    private int _plantLevel;
+
+    public Plant()
+    {
+      _plantHealth = 10;
+      _plantLevel = 1;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Grow.BasicNeeds;
+using Grow.Models;
 
 namespace Grow
 {
